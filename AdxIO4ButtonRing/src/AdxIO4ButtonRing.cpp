52ffcaa45a9d25ed2ba1f2f82a2aaf5c836ea0be
istareatscreens <s.charb@gmail.com>
2024-10-20 06:59:06 -0400

    Add dependencies for HIDAPI to build properly

--- AdxIO4ButtonRing/src/AdxIO4ButtonRing.cpp
+++ AdxIO4ButtonRing/src/AdxIO4ButtonRing.cpp
@@ -4,11 +4,14 @@
 
 bool AdxIO4ButtonRing::connect()
 {
+    std::cout << "HELLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOO";
     struct hid_device_info *devs, *cur_dev;
 
     ConnectionProperties connection_properties;
 
     devs = hid_enumerate(connection_properties.vender_id, connection_properties.product_id);
+
+    std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << devs->manufacturer_string;
 
     return true;
 }
